e2c2363aa926a14cf7dd282c2ec005748419ea71
sloan58 <martin.sloan@karma-tek.com>
2014-10-27 22:07:48 -0400

    update mail config

--- app/config/mail.php
+++ app/config/mail.php
@@ -2,83 +2,83 @@
 
 return array(
 
-	/*
-	|--------------------------------------------------------------------------
-	| Mail Driver
-	|--------------------------------------------------------------------------
-	|
-	| Laravel supports both SMTP and PHP's "mail" function as drivers for the
-	| sending of e-mail. You may specify which one you're using throughout
-	| your application here. By default, Laravel is setup for SMTP mail.
-	|
-	| Supported: "smtp", "mail"
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Mail Driver
+    |--------------------------------------------------------------------------
+    |
+    | Laravel supports both SMTP and PHP's "mail" function as drivers for the
+    | sending of e-mail. You may specify which one you're using throughout
+    | your application here. By default, Laravel is setup for SMTP mail.
+    |
+    | Supported: "smtp", "mail"
+    |
+    */
 
-	'driver' => 'smtp',
+    'driver' => 'smtp',
 
-	/*
-	|--------------------------------------------------------------------------
-	| SMTP Host Address
-	|--------------------------------------------------------------------------
-	|
-	| Here you may provide the host address of the SMTP server used by your
-	| applications. A default option is provided that is compatible with
-	| the Postmark mail service, which will provide reliable delivery.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | SMTP Host Address
+    |--------------------------------------------------------------------------
+    |
+    | Here you may provide the host address of the SMTP server used by your
+    | applications. A default option is provided that is compatible with
+    | the Postmark mail service, which will provide reliable delivery.
+    |
+    */
 
-	'host' => 'smtp.postmarkapp.com',
+    'host' => 'smtp.gmail.com',
 
-	/*
-	|--------------------------------------------------------------------------
-	| SMTP Host Port
-	|--------------------------------------------------------------------------
-	|
-	| This is the SMTP port used by your application to delivery e-mails to
-	| users of your application. Like the host we have set this value to
-	| stay compatible with the Postmark e-mail application by default.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | SMTP Host Port
+    |--------------------------------------------------------------------------
+    |
+    | This is the SMTP port used by your application to delivery e-mails to
+    | users of your application. Like the host we have set this value to
+    | stay compatible with the Postmark e-mail application by default.
+    |
+    */
 
-	'port' => 2525,
+    'port' => 465,
 
-	/*
-	|--------------------------------------------------------------------------
-	| Global "From" Address
-	|--------------------------------------------------------------------------
-	|
-	| You may wish for all e-mails sent by your application to be sent from
-	| the same address. Here, you may specify a name and address that is
-	| used globally for all e-mails that are sent by your application.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Global "From" Address
+    |--------------------------------------------------------------------------
+    |
+    | You may wish for all e-mails sent by your application to be sent from
+    | the same address. Here, you may specify a name and address that is
+    | used globally for all e-mails that are sent by your application.
+    |
+    */
 
-	'from' => array('address' => null, 'name' => null),
+    'from' => array('address' => 'martinsloan58@gmail.com', 'name' => 'Martin Sloan'),
 
-	/*
-	|--------------------------------------------------------------------------
-	| E-Mail Encryption Protocol
-	|--------------------------------------------------------------------------
-	|
-	| Here you may specify the encryption protocol that should be used when
-	| the application send e-mail messages. A sensible default using the
-	| transport layer security protocol should provide great security.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | E-Mail Encryption Protocol
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the encryption protocol that should be used when
+    | the application send e-mail messages. A sensible default using the
+    | transport layer security protocol should provide great security.
+    |
+    */
 
-	'encryption' => 'tls',
+    'encryption' => 'ssl',
 
-	/*
-	|--------------------------------------------------------------------------
-	| SMTP Server Username
-	|--------------------------------------------------------------------------
-	|
-	| If your SMTP server requires a username for authentication, you should
-	| set it here. This will get used to authenticate with your server on
-	| connection. You may also set the "password" value below this one.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | SMTP Server Username
+    |--------------------------------------------------------------------------
+    |
+    | If your SMTP server requires a username for authentication, you should
+    | set it here. This will get used to authenticate with your server on
+    | connection. You may also set the "password" value below this one.
+    |
+    */
 
     'username' => getenv('MAIL_USERNAME'),
 
