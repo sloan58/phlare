82d13a2ac4cbadb002d2b82efcfc5f1d9cb026a8
sloan58 <martin.sloan@karma-tek.com>
2014-10-28 10:59:38 -0400

    update index page wording

--- app/views/site/index.blade.php
+++ app/views/site/index.blade.php
@@ -16,18 +16,19 @@
         <div class="row">
             <div class="col-lg-4">
                 <h2>What is Phlare?</h2>
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                <p>Phlare is a place to store your contacts so that the information is available to you whenever you need it.</p>
                 <p><a class="btn btn-primary" href="#" role="button">View details »</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>What is the cost?</h2>
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                <h2>How does it work?</h2>
+                <p>Phlare synchronizes with your smart device and stores the names and telephone numbers so you can reach your contacts anytime. </p>
                 <p><a class="btn btn-primary" href="#" role="button">View details »</a></p>
             </div>
             <div class="col-lg-4">
                 <h2>How can I get it?</h2>
-                <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
-                <p><a class="btn btn-primary" href="#" role="button">View details »</a></p>
+                <div class="col-md-2 col-md-offset-2">
+                    <p><a class="btn btn-success" href={{{ URL::to('user/create') }}} role="button">Sign Up!</a></p>
+                </div>
             </div>
         </div>
     </div>
