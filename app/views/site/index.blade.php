095a26a68368206c7ac8715de51c922cfed64713
sloan58 <martin.sloan@karma-tek.com>
2014-10-28 12:21:24 -0400

    update index page

--- app/views/site/index.blade.php
+++ app/views/site/index.blade.php
@@ -16,16 +16,16 @@
         <div class="row">
             <div class="col-lg-4">
                 <h2>What is Phlare?</h2>
-                <p>Phlare is a place to store your contacts so that the information is available to you whenever you need it.</p>
+                <p></p>
                 <p><a class="btn btn-primary" href="#" role="button">View details »</a></p>
             </div>
             <div class="col-lg-4">
                 <h2>How does it work?</h2>
-                <p>Phlare synchronizes with your smart device and stores the names and telephone numbers so you can reach your contacts anytime. </p>
+                <p></p>
                 <p><a class="btn btn-primary" href="#" role="button">View details »</a></p>
             </div>
             <div class="col-lg-4">
-                <h2>How can I get it?</h2>
+                <h2></h2>
                 <div class="col-md-2 col-md-offset-2">
                     <p><a class="btn btn-success" href={{{ URL::to('user/create') }}} role="button">Sign Up!</a></p>
                 </div>
