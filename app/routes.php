b11517899dcbe88457d2382a7b0bd226c8c58ece
sloan58 <martin.sloan@karma-tek.com>
2014-10-28 23:19:42 -0400

    twilio update

--- app/routes.php
+++ app/routes.php
@@ -90,7 +90,41 @@ Route::group(array('before'=>'auth'), function() {
 });
 
 #Twilio Route
-Route::resource('twilio', 'TwilioController');
+//Route::resource('twilio', 'TwilioController');
+Route::get('/twilio2', function()
+{
+
+    // Create an authenticated client for the Twilio API
+    $client = new Services_Twilio('AC685a150b41abf533cae4bca0319d3c0b', 'd2a703623efd92b7fe1d980bf1e812d4');
+
+    try {
+
+        // Use the Twilio REST API client to send a text message
+        $m = $client->account->messages->sendMessage(
+            '+12025099421', // the text will be sent from your Twilio number
+            '2028055054', // the phone number the text will be sent to
+            'Here is a message!' // the body of the text message
+        );
+
+    } catch (Services_Twilio_RestException $E) {
+
+        return $E;
+
+    }
+
+    // Return the message object to the browser as JSON
+    return $m;
+
+});
+
+Route::match(array('GET', 'POST'), '/twilio', function()
+{
+    $twiml = new Services_Twilio_Twiml();
+    $twiml->say('Hello - your app just answered the phone. Neat, eh?', array('voice' => 'alice'));
+    $response = Response::make($twiml, 200);
+    $response->header('Content-Type', 'text/xml');
+    return $response;
+});
 
 # Index Page - Last route, no matches
 Route::get('/', array('before' => 'detectLang','auth','uses' => 'MainController@getIndex'));
