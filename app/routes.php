d4cd18ac794907b38cdceb4b04e155bc4f40c991
sloan58 <martin.sloan@karma-tek.com>
2014-10-29 23:01:27 -0400

    add dialing to twilio routes

--- app/routes.php
+++ app/routes.php
@@ -94,8 +94,8 @@ Route::group(array('before'=>'auth'), function() {
 Route::get('twilio', function () {
 
     $twiml = new Services_Twilio_Twiml();
-    $gather = $twiml->gather(array('numDigits' => 50));
-    $gather->say("Hello Caller, enter the name of the person you're trying to reach, followed by the pound sign.");
+    $gather = $twiml->gather(['numDigits' => 50]);
+    $gather->say("Hello Caller, please enter the name of the person you're trying to reach, followed by the pound sign.");
     return $twiml;
 
 });
@@ -104,13 +104,22 @@ Route::post('twilio', function () {
 
     $digits = Input::get('Digits');
 
-    $name = DB::table('contacts')->where('dial_profile',$digits)->pluck('name');
+    $fetch = Contact::where('dial_profile', 'LIKE', $digits .'%')->get();
 
-    $twiml = new Services_Twilio_Twiml();
-    $twiml->say('Okay, I got the name ' . $name . ' from the database based on the digits you pressed!  Goodbye.');
+    $contact = $fetch[0];
+
+    $spoken_num = implode(' ', str_split($contact->number));
+
+    $twiml = new Services_Twilio_Twiml;
+    $twiml->say('Okay, I found your contact ' . $contact->name . ' based on the digits you pressed!  I\'ll call them at ' . $spoken_num . '.');
+    $twiml->dial('+1' . $contact->number , array(
+        'callerId' => '+12025099421',
+    ));
+
     $response = Response::make($twiml, 200);
     $response->header('Content-Type', 'text/xml');
     return $response;
+
 
 });
 
