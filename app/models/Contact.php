37c183ba9f6012e31580ccf0c1f92a13cef6329a
sloan58 <martin.sloan@karma-tek.com>
2014-11-01 10:16:11 -0400

    remove Apiv1Controller

--- app/models/Contact.php
+++ app/models/Contact.php
@@ -3,4 +3,12 @@
 class Contact extends Eloquent
 {
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function contacts()
+    {
+        return $this->hasMany('Contact');
+    }
+
 }
